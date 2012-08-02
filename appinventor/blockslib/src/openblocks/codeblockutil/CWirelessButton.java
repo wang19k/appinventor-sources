cf002f3077c0b57a61f8d31effb3696e114d8bea
Jeffrey I. Schiller <jis@mit.edu>
2012-08-01 21:16:38 -0400

    Modified indentation of CWirelessButton.java to conform to standard indentation.

--- appinventor/blockslib/src/openblocks/codeblockutil/CWirelessButton.java
+++ appinventor/blockslib/src/openblocks/codeblockutil/CWirelessButton.java
@@ -1,5 +1,5 @@
+// -*- mode: java; c-basic-offset: 2; -*-
 package openblocks.codeblockutil;
-
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -48,132 +48,132 @@ import openblocks.yacodeblocks.PhoneCommManager;
  */
 public class CWirelessButton extends CSaveButton{
 
-	private static final String YAIL_NEWLINE = "(newline)"; // Cribbed from PhoneCommManager
-	private static final String REPL_CONFIRMATION = "Confirmation"; // ditto
+  private static final String YAIL_NEWLINE = "(newline)"; // Cribbed from PhoneCommManager
+  private static final String REPL_CONFIRMATION = "Confirmation"; // ditto
 
-	private WorkspaceController wc;
+  private WorkspaceController wc;
 
-	public CWirelessButton() {
-		super("Connect Wifi", "can't start connection");
-	}
+  public CWirelessButton() {
+    super("Connect Wifi", "can't start connection");
+  }
 
-	private static JFrame frame;
-	private static final String AB = "0123456789abcdefghijklmnopqrstuvwxyz";
-	private static Random rnd = new Random();
-	public final String CONNECTING_MSG = randomString(5);
-	public final String theUrl = "http://osiris.mit.edu/rendezvous/";
-	private PhoneCommManager pcm; // When the OK option is selected from the message dialog we use this to start the REPL
-        private ImageIcon qrcode = new ImageIcon();
+  private static JFrame frame;
+  private static final String AB = "0123456789abcdefghijklmnopqrstuvwxyz";
+  private static Random rnd = new Random();
+  public final String CONNECTING_MSG = randomString(5);
+  public final String theUrl = "http://osiris.mit.edu/rendezvous/";
+  private PhoneCommManager pcm; // When the OK option is selected from the message dialog we use this to start the REPL
+  private ImageIcon qrcode = new ImageIcon();
 
-	String randomString(int len)
-	{
-		StringBuilder sb = new StringBuilder(len);
-		for(int i=0; i<len; i++)
-			sb.append(AB.charAt(rnd.nextInt(AB.length())));
-		return sb.toString();
-	}
+  String randomString(int len)
+  {
+    StringBuilder sb = new StringBuilder(len);
+    for(int i=0; i<len; i++)
+      sb.append(AB.charAt(rnd.nextInt(AB.length())));
+    return sb.toString();
+  }
 
-	public void init() {
-	    addActionListener(new ActionListener() {
-		@Override
-		public void actionPerformed(ActionEvent e) {
-		  javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			  public void run() {
-			      wc = (WorkspaceController) WorkspaceControllerHolder.get();
-			      showWirelessNotice();
-			      System.out.println("Let's show that notice!");
-			      try {
-				  System.out.println("Well, I tried.");
-				  URL url = new URL(theUrl + CONNECTING_MSG);
-				  URLConnection con = url.openConnection();
-				  System.out.println("Opening a URL connection");
-				  InputStream in = con.getInputStream();
-				  System.out.println("Well, input stream worked fine.");
-				  BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-				  System.out.println("BufferedReader worked fine.");
-				  String jsonString = reader.readLine();
-				  while (jsonString == null) {
-				      url = new URL(theUrl + CONNECTING_MSG);
-				      con = url.openConnection();
-				      System.out.println("Opening a URL connection");
-				      in = con.getInputStream();
-				      System.out.println("Well, input stream worked fine.");
-				      reader = new BufferedReader(new InputStreamReader(in));
-				      System.out.println("BufferedReader worked fine.");
-				      jsonString = reader.readLine();
-				      System.out.println("JSON read the line");
-				      Thread.sleep(100);
-				  }
-				  System.out.println("Cool, it stopped being NULL and connected");
-				  JSONObject jsonObject = new JSONObject(jsonString);
-				  System.out.println("Made the JSON object");
-				  String ipAddress = (String) jsonObject.get("ipaddr");
-				  System.out.println("CWirelessButton: ipaddr = " + ipAddress);
-				  WifiAndroidController ac = new WifiAndroidController();
-				  ac.setIpAddress(ipAddress);
-				  wc.setupWirelessConnection(ac, ipAddress);
-				  pcm = wc.getPhoneCommManager();
-				  pcm.setAndroidController(ac);
-				  pcm.setConnectedToPhone(true); // Kludge -- no USB cable, so implicitly connected
-				  System.out.println("CWirelessButton: Set Connected to Phone.");
-				  pcm.initReplController();
-				  pcm.replControllerCreateAndSendAsync(YAIL_NEWLINE, REPL_CONFIRMATION, new Long(0), false);
-			      } catch(Exception e) {
-				  System.out.println("It did not work." + e.toString());//return
-				  e.printStackTrace(System.out);
-			      }
+  public void init() {
+    addActionListener(new ActionListener() {
+        @Override
+          public void actionPerformed(ActionEvent e) {
+          javax.swing.SwingUtilities.invokeLater(new Runnable() {
+              public void run() {
+                wc = (WorkspaceController) WorkspaceControllerHolder.get();
+                showWirelessNotice();
+                System.out.println("Let's show that notice!");
+                try {
+                  System.out.println("Well, I tried.");
+                  URL url = new URL(theUrl + CONNECTING_MSG);
+                  URLConnection con = url.openConnection();
+                  System.out.println("Opening a URL connection");
+                  InputStream in = con.getInputStream();
+                  System.out.println("Well, input stream worked fine.");
+                  BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                  System.out.println("BufferedReader worked fine.");
+                  String jsonString = reader.readLine();
+                  while (jsonString == null) {
+                    url = new URL(theUrl + CONNECTING_MSG);
+                    con = url.openConnection();
+                    System.out.println("Opening a URL connection");
+                    in = con.getInputStream();
+                    System.out.println("Well, input stream worked fine.");
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    System.out.println("BufferedReader worked fine.");
+                    jsonString = reader.readLine();
+                    System.out.println("JSON read the line");
+                    Thread.sleep(100);
+                  }
+                  System.out.println("Cool, it stopped being NULL and connected");
+                  JSONObject jsonObject = new JSONObject(jsonString);
+                  System.out.println("Made the JSON object");
+                  String ipAddress = (String) jsonObject.get("ipaddr");
+                  System.out.println("CWirelessButton: ipaddr = " + ipAddress);
+                  WifiAndroidController ac = new WifiAndroidController();
+                  ac.setIpAddress(ipAddress);
+                  wc.setupWirelessConnection(ac, ipAddress);
+                  pcm = wc.getPhoneCommManager();
+                  pcm.setAndroidController(ac);
+                  pcm.setConnectedToPhone(true); // Kludge -- no USB cable, so implicitly connected
+                  System.out.println("CWirelessButton: Set Connected to Phone.");
+                  pcm.initReplController();
+                  pcm.replControllerCreateAndSendAsync(YAIL_NEWLINE, REPL_CONFIRMATION, new Long(0), false);
+                } catch(Exception e) {
+                  System.out.println("It did not work." + e.toString());//return
+                  e.printStackTrace(System.out);
+                }
 
-			  }
-		      });
-		}});
-	}
+              }
+            });
+        }});
+  }
 
-	private void showWirelessNotice() {
-		String title = "Starting the wireless connection.";
-		String msgText = "This is your 5 digit code: " + CONNECTING_MSG;
-		generateQRCode();
-		if (qrcode == null)
-		    System.out.println ("LOLOL it is null");
-		else
-		    System.out.println ("Looks like qrcode does something");
-		FeedbackReporter.showInfoMessage(msgText, qrcode);
-		
-	}
+  private void showWirelessNotice() {
+    String title = "Starting the wireless connection.";
+    String msgText = "This is your 5 digit code: " + CONNECTING_MSG;
+    generateQRCode();
+    if (qrcode == null)
+      System.out.println ("LOLOL it is null");
+    else
+      System.out.println ("Looks like qrcode does something");
+    FeedbackReporter.showInfoMessage(msgText, qrcode);
 
-	private void generateQRCode() {
-	    Charset charset = Charset.forName("ISO-8859-1");
-	    CharsetEncoder encoder = charset.newEncoder();
-	    byte[] b = null;
-	    try {
-	        // Convert a string to ISO-8859-1 bytes in a ByteBuffer
-	        ByteBuffer bbuf = encoder.encode(CharBuffer.wrap(CONNECTING_MSG));
-	        b = bbuf.array();
-	    } catch (CharacterCodingException e) {
-	        System.out.println(e.getMessage());
-	    }
+  }
 
-	    String data = null;
-	    try {
-	        data = new String(b, "ISO-8859-1");
-	    } catch (UnsupportedEncodingException e) {
-	        System.out.println(e.getMessage());
-	    }
+  private void generateQRCode() {
+    Charset charset = Charset.forName("ISO-8859-1");
+    CharsetEncoder encoder = charset.newEncoder();
+    byte[] b = null;
+    try {
+      // Convert a string to ISO-8859-1 bytes in a ByteBuffer
+      ByteBuffer bbuf = encoder.encode(CharBuffer.wrap(CONNECTING_MSG));
+      b = bbuf.array();
+    } catch (CharacterCodingException e) {
+      System.out.println(e.getMessage());
+    }
 
-	    // get a byte matrix for the data
-	    BitMatrix matrix = null;
-	    int h = 200;
-	    int w = 200;
-	    com.google.zxing.Writer writer = new QRCodeWriter();
-	    try {
-	        matrix = writer.encode(data,
-	        com.google.zxing.BarcodeFormat.QR_CODE, w, h);
-	    } catch (Exception e) {
-	        System.out.println(e.getMessage());
-	    }
-	    try {
-	        qrcode.setImage(MatrixToImageWriter.toBufferedImage(matrix));
-	    } catch (Exception e) {
-	        System.out.println(e.getMessage());
-	    }
-	}
+    String data = null;
+    try {
+      data = new String(b, "ISO-8859-1");
+    } catch (UnsupportedEncodingException e) {
+      System.out.println(e.getMessage());
+    }
+
+    // get a byte matrix for the data
+    BitMatrix matrix = null;
+    int h = 200;
+    int w = 200;
+    com.google.zxing.Writer writer = new QRCodeWriter();
+    try {
+      matrix = writer.encode(data,
+                             com.google.zxing.BarcodeFormat.QR_CODE, w, h);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    try {
+      qrcode.setImage(MatrixToImageWriter.toBufferedImage(matrix));
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
 }
