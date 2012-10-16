c6076db0b3fc386d898850b70e9a8ce06371468b
Jeffrey I. Schiller <jis@mit.edu>
2012-10-15 21:14:55 -0400

    Cleanup PhoneStatus.java
    Change-Id: I33775b7e9c307ca4ad4819a97b4e3ace595900d8

--- appinventor/components/src/com/google/appinventor/components/runtime/PhoneStatus.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/PhoneStatus.java
@@ -1,3 +1,8 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2009-2011 Google, All Rights reserved
+// Copyright 2011-2012 MIT, All rights reserved
+// Released under the MIT License https://raw.github.com/mit-cml/app-inventor/master/mitlicense.txt
+
 package com.google.appinventor.components.runtime;
 
 import android.app.Activity;
@@ -13,7 +18,11 @@ import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.common.YaVersion;
 
 /**
- * This component keeps count of steps using the accelerometer.
+ * Component for obtaining Phone Information. Currently supports
+ * obtaining the IP Address of the phone and whether or not it is
+ * connected via a WiFi connection.
+ *
+ * @author lmercer@mit.edu (Logan Mercer)
  *
  */
 @DesignerComponent(version = YaVersion.PHONESTATUS_COMPONENT_VERSION,
@@ -22,43 +31,41 @@ import com.google.appinventor.components.common.YaVersion;
                    nonVisible = true,
                    iconName = "images/pedometer.png")
 @SimpleObject
-
 public class PhoneStatus extends AndroidNonvisibleComponent implements Component {
-	
-	private static Activity activity;
-	
-	  public PhoneStatus(ComponentContainer container) {
-		super(container.$form());
-		activity = container.$context();
-	}
 
-	  @SimpleFunction(description = "Returns the IP address of the phone in the form of a String")
-	  public static String GetWifiIpAddress(){
-		  DhcpInfo ip;
-		  Object wiman = activity.getSystemService("wifi");
-		  ip = ((WifiManager) wiman).getDhcpInfo();
-	      int s_ipAddress=(ip.ipAddress);
-	      String whatWeWant;
-	      if (isConnected())
-	    	  whatWeWant = intToIp(s_ipAddress);
-	      else
-	    	  whatWeWant = "Error: No Wifi Connection";
-		  return whatWeWant;
-	  }
-	  
-	  @SimpleFunction(description = "Returns TRUE if the phone is on Wifi, FALSE otherwise")
-	  public static boolean isConnected() {
-		    ConnectivityManager connectivityManager = (ConnectivityManager) activity.getSystemService("connectivity");
-		    NetworkInfo networkInfo = null;
-		    if (connectivityManager != null) {
-		        networkInfo =
-		            connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-		    }
-		    return networkInfo == null ? false : networkInfo.isConnected();
-		}
-	  
-	  public static String intToIp(int i) {
+  private static Activity activity;
 
-	  		return (i & 0xFF) + "." + ((i >> 8) & 0xFF) + "." + ((i >> 16) & 0xFF) + "." + ((i >>24) & 0xFF);
-		}
+  public PhoneStatus(ComponentContainer container) {
+    super(container.$form());
+    activity = container.$context();
+  }
+
+  @SimpleFunction(description = "Returns the IP address of the phone in the form of a String")
+  public static String GetWifiIpAddress() {
+    DhcpInfo ip;
+    Object wifiManager = activity.getSystemService("wifi");
+    ip = ((WifiManager) wifiManager).getDhcpInfo();
+    int s_ipAddress= ip.ipAddress;
+    String ipAddress;
+    if (isConnected())
+      ipAddress = intToIp(s_ipAddress);
+    else
+      ipAddress = "Error: No Wifi Connection";
+    return ipAddress;
+  }
+
+  @SimpleFunction(description = "Returns TRUE if the phone is on Wifi, FALSE otherwise")
+  public static boolean isConnected() {
+    ConnectivityManager connectivityManager = (ConnectivityManager) activity.getSystemService("connectivity");
+    NetworkInfo networkInfo = null;
+    if (connectivityManager != null) {
+      networkInfo =
+        connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+    }
+    return networkInfo == null ? false : networkInfo.isConnected();
+  }
+
+  public static String intToIp(int i) {
+    return (i & 0xFF) + "." + ((i >> 8) & 0xFF) + "." + ((i >> 16) & 0xFF) + "." + ((i >>24) & 0xFF);
+  }
 }
